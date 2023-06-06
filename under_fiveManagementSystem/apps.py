f2f46e162ffe9593d7cbe101fbcf20839b99c9f7
sangwaniKondowe <josephgmkonda@gmail.com>
2023-06-06 16:18:52 +0400

    first commit

---
+++ under_fiveManagementSystem/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class UnderFivemanagementsystemConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'under_fiveManagementSystem'
