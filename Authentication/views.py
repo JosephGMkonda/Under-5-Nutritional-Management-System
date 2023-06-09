ea7d8ca3bc4856c74c8fc6783c4642ce84d2b2ec
sangwaniKondowe <josephgmkonda@gmail.com>
2023-06-09 19:28:47 +0400

    setting up templates

--- Authentication/views.py
+++ Authentication/views.py
@@ -1,7 +1,3 @@
 from django.shortcuts import render
 
 # Create your views here.
-
-
-def home(request):
-    return render(request, 'base.html')
--- under_5Nutritional/settings.py
+++ under_5Nutritional/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'under_fiveManagementSystem'
+    'under_fiveManagementSystem',
+    'Authentication'
 ]
 
 MIDDLEWARE = [
