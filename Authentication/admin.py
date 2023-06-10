cffacd091036831b2bb9c3a417daf77e6a90f130
sangwaniKondowe <josephgmkonda@gmail.com>
2023-06-10 12:34:45 +0400

    ChildDetails Model update

--- Authentication/admin.py
+++ Authentication/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import ChildDetails
 
 # Register your models here.
+class AgeModel(admin.ModelAdmin):
+    list_display=('firstname','lastname','get_age',)
+
+admin.site.register(ChildDetails,AgeModel)
+
+
+
